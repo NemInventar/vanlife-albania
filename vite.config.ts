1e2cca5d4b5e59c0bbc0b2fba24ea09f918ae080
Milot Salihu <milots2@MacBook-Air-tilhrende-Milot.local>
2026-04-22 13:54:48 +0200

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -208,6 +208,7 @@ function cdnPrefixImages(): Plugin {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    base: mode === 'production' ? '/vanlife-albania/' : '/',
     server: {
       host: "::",
       port: 8080,
